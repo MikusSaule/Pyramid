420d93e3389e11ee25be0f23ee57427e26fd7d82
MikusSaule <109538364+MikusSaule@users.noreply.github.com>
2022-08-21 19:28:55 +0300

    Create Pyramid.java

---
+++ Pyramid.java
@@ -0,0 +1,8 @@
+ic class Main {
+    public static void main(String[] args) {
+        String[] pyramid = {"    *  ","   * * ","  * * * "," * * * *","* * * * *"};
+        for (int i = 0; i < pyramid.length; i++){
+            System.out.println(pyramid[i]);
+        }
+    }
+}
